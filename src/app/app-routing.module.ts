6a5d919f856668dd5322bb32f7cc7ed0e1612a60
BenJackGuru <100480556+BenJackGuru@users.noreply.github.com>
2022-07-22 13:10:32 +0530

    container-1 home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'', redirectTo:'home',pathMatch:'full'},
+  {path:'product', component:ProfileComponent},
+  {path:'blog', component:BlogComponent},
+  {path:'about', component:AboutComponent},
+  {path:'contact', component:ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
